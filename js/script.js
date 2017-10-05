144743a26dfb9f4bd6350189374864075c4f4860
Pedro Monteiro <pedrormonteiro@hotmail.com>
2017-10-05 15:20:42 -0300

    Adding code files

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//Se Deus não existe e você é ateu = tanto faz.
+//Se Deus existe e você é ateu = inferno.
+//Se Deus não existe e você acredita em Deus = tanto faz.
+//Se Deus existe e você acredita em Deus = céu.
+//
+//Ou seja, ser ateu é ser burro.
+//function isAteu(god, atheist){
+//    if(god && atheist){
+//     return 'hell';
+//    }else if( (!god && atheist) || (!god && !atheist) ){
+//     return 'whatever';
+//    }else{
+//     return 'heaven';
+//    }
+//}
+//
+//isAteu(true, false);
+
